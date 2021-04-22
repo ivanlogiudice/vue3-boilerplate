e740fa4656bf8f1d6e9db796f6cf05336b3037fc
Iván Máximiliano, Lo Giudice <ivanmaxlogiudice@gmail.com>
2021-04-21 22:37:23 -0300

    ESlint + Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint'
+    ],
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2020
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'space-before-function-paren': ['error', 'never'],
+        'comma-dangle': ['error', 'never'],
+
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off'
+    }
+}
